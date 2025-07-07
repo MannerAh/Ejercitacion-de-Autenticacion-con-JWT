a22003c9e8d6ac2fd3c1f509cec41c6e9dc122fb
MannerAh <leoncitobaro@hotmail.es>
2025-07-07 13:39:22 -0300

    Mongoose, env, and database config, index staging.

---
+++ controllers/logicas.js
@@ -0,0 +1,3 @@
+const jwt = require('jsonwebtoken');
+auth = require('../middleware/authToken');
+const { connectDB } = require('../config/database');
---
+++ index.js
@@ -0,0 +1,24 @@
+// Importación de dependencias
+const express = require('express');
+const { use } = require('react');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Creación de la aplicación
+
+app-use(express.json());
+
+// Rutas
+
+// POST Register
+app.use
+
+// POST Login
+
+// GET Tareas?
+
+// GET 
+
+app.listen(3000, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
